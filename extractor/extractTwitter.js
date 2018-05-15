fa18e7d5d0c1def9c13a3aa5c978ad81d4e7f052
Felix Moenckemeyer <felix.moenckemeyer@rwth-aachen.de>
2018-05-15 20:27:43 +0200

    twitter environment

--- extractor/extractTwitter.js
+++ extractor/extractTwitter.js
@@ -1,11 +1,11 @@
 var Twitter = require('twitter');
  
 var client = new Twitter({
-  consumer_key: '4q8ywpbx8YwvCInQm7Ep6AVUu',
-  consumer_secret: '9JgCR49bSHw9Z8uG68pE6qgpvibdllcRsbxGjkj1qm8NYpnpmh',
-  access_token_key: '994599121205059584-LzmoFVumQspZJ6j8haBGXCGa107R7cw',
-  access_token_secret: 'aReV4dKvaRAXlezNN5tnU5wlbn0jJIVFkMuf6MgWBckXO'
-});
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
  
 var stream = client.stream('statuses/filter', {track: 'javascript'});
 stream.on('data', function(event) {
