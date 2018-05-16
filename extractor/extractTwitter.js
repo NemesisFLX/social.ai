ab67d8ac1599c364617c27b801b53a3269ad8e7a
Felix Moenckemeyer <felix.moenckemeyer@rwth-aachen.de>
2018-05-16 17:15:06 +0200

    first try walking

--- extractor/extractTwitter.js
+++ extractor/extractTwitter.js
@@ -1,6 +1,11 @@
 var Twitter = require('twitter');
 var fs = require('fs');
-
+var MongoClient = require("mongodb").MongoClient;
+var CONFIG = require("./config.json");
+var dbName = CONFIG.dbName;
+var dbPort = CONFIG.dbPort;
+var dbHost = CONFIG.dbHost;
+var url = "mongodb://" + dbHost + ":" + dbPort;
  
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -12,9 +17,35 @@ var client = new Twitter({
 var stream = client.stream('statuses/filter', {track: 'javascript'});
 stream.on('data', function(event) {
   console.log(event.user.followers_count + " | " + event.id_str + " | " + event.created_at + " | " + event.user.screen_name);
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+        throw err;
+    }
+    var dbo = db.db(dbName);
+    dbo.collection(dbCollection).insertOne(createJSON(msg, pairJSON), function (err, res) {
+        if (err) {
+            throw err;
+        }
+    });
+    db.close();
+  });
+  console.log(event.user.followers_count + " saved!")
 });
  
 stream.on('error', function(error) {
   throw error;
 });
 
+MongoClient.connect(url, function(err, db) {
+  if (err) {
+      throw err;
+  }
+  var dbo = db.db(dbName);
+  dbo.createCollection(dbCollection, function(err, res) {
+      if (err) {
+          throw err;
+      }
+      //console.log("Collection created!");
+      db.close();
+  });
+});
