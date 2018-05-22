2bbed53be0ee0fc9ec160d3df21ceee4a5b22a62
K_Herrmann <kristof.herrmann@gmx.net>
2018-05-23 00:41:41 +0200

    added functios to message (getters) and the creation of the array of occuring words in the first n entries

--- tools/message.py
+++ tools/message.py
@@ -1,6 +1,6 @@
 from tools.connectMDB import get_by_ID
 from tools.connectMDB import get_one_by_ID
-
+from tools.connectMDB import get_collection
 
 class message(object):
 
@@ -37,9 +37,48 @@ class message(object):
         self.contributors = entry['contributors']
         self.is_quote_status = entry['is_quote_status']
 
+    def get_text(self):
+        return self.text,self.text.split(),self.text.split().__len__()
+
+
+def get_array_of_words(len = 10,CLEAN = True):
+    messages = []
+    clean = []
+    i = 0
+    if CLEAN ==True:
+        for entry in get_collection().find():
+            m = message(entry["id"])
+            clean.extend(get_clean_text(m))
+            messages.append(message)
+            i = i+1
+            print(i)
+            if i >len:
+                break
+    else:
+        for entry in get_collection().find():
+            m = message(entry["id"])
+            __,arr,__ =m.get_text()
+            clean.extend(arr)
+            messages.append(message)
+            i = i +1
+            if i >len:
+                break
+    return clean,messages
+
+
+def get_clean_text(message = message, options=['www','https']):
+    __,arr,__ = message.get_text()
+    for opt in options:
+        for word in arr:
+            if opt in word:
+                arr.remove(word)
+    return arr
+
+
 
 if __name__ == "__main__":
     # 9.967880685930947e+17 is the ID of the first entry in the Dataset
     m = message(9.967880685930947e+17)
+
     for key in m.quote_count.keys():
         print("Timestamp in ms: " + key + "  |  No. of quotes: " + str(m.quote_count[key]))
